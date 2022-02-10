2e99f6eacfe91979c0e05ed6edf82768f78f1ff0
david curtis <heythereimdavidcurtis@gmail.com>
2022-02-10 14:35:40 -0500

    cleaning up code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,13 @@
 var searchFormEl = document.getElementById("search-form");
 var cityInputEl = document.getElementById("city");
-var cityNameEl = document.getElementById("city-name-header");
-var tempEl = document.getElementById("temp");
-var windEl = document.getElementById("wind");
-var humidityEl = document.getElementById("humidity");
-var uVIndexEl = document.getElementById("uv-index");
+// var cityNameEl = document.getElementById("city-name-header");
+// var tempEl = document.getElementById("temp");
+// var windEl = document.getElementById("wind");
+// var humidityEl = document.getElementById("humidity");
+// var uVIndexEl = document.getElementById("uv-index");
 var dailyEl = document.getElementById("daily-row");
+
+var date = moment().format("l");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -21,65 +23,40 @@ var formSubmitHandler = function (event) {
   }
 };
 
-// var getCityWeather = function (cityname) {
-//   var apiUrl =
-//     "https://api.openweathermap.org/data/2.5/weather?q=" +
-//     cityname +
-//     "&units=imperial&appid=28da97f6c34c725f70e79aba45588612";
-
-//   fetch(apiUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           console.log(data);
-//           //   displayCityWeather();
-//           cityNameEl.innerHTML = data.name;
-//           tempEl.innerHTML = "Temp: " + data.main.temp + " Fahrenheit";
-//           windEl.innerHTML = "Wind: " + data.wind.speed + " MPH";
-//           humidityEl.innerHTML = "Humidity: " + data.main.humidity + " %";
-//           //   uVIndexEl.innerHTML = data.
-//         });
-//       } else {
-//         alert("Error: " + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert("Unable to connect to Openweathermap");
-//     });
-// };
-
-var getCityWeather = function (cityname) {
-  var apiUrl =
+let getCityWeather = function (cityname) {
+  let apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityname +
     "&units=imperial&appid=28da97f6c34c725f70e79aba45588612";
-
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
+        lat = data.coord.lat;
+        lon = data.coord.lon;
+        cityName = data.name;
+        displayCityWeather();
+        return lat + lon + cityName;
+      });
+    }
+  });
+};
+
+let displayCityWeather = function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=hourly&units=imperial&appid=28da97f6c34c725f70e79aba45588612"
+  ).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
         console.log(data);
-        var date = moment().format("l");
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        cityNameEl.innerHTML = data.name + " " + "(" + date + ")";
-        fetch(
-          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-            lat +
-            "&lon=" +
-            lon +
-            "&exclude=hourly&units=imperial&appid=28da97f6c34c725f70e79aba45588612"
-        ).then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-              console.log(data);
-              tempEl.innerHTML = "Temp: " + data.current.temp + " Fahrenheit";
-              windEl.innerHTML = "Wind: " + data.current.wind_speed + " MPH";
-              humidityEl.innerHTML =
-                "Humidity: " + data.current.humidity + " %";
-              uVIndexEl.innerHTML = "UV Index: " + data.current.uvi;
-            });
-          }
-        });
+        $("#city-name-header").text(cityName + " (" + date + ")");
+        $("#temp").text("Temp: " + data.current.temp + " Fahrenheit");
+        $("#wind").text("Wind: " + data.current.wind_speed + " MPH");
+        $("#humidity").text("Humidity: " + data.current.humidity + " %");
+        $("#uv-index").text("UV Index: " + data.current.uvi);
       });
     }
   });
